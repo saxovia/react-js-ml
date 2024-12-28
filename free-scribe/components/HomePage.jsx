5bbea5644cbdf46ddd4f054886a90d28859327ce
j99lzt <liliszasz1@gmail.com>
2024-12-29 00:04:38 +0100

    Add progress

--- free-scribe/components/HomePage.jsx
+++ free-scribe/components/HomePage.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 
-export default function HomePage() {
+export default function HomePage(props) {
+    const {setAudioStream, setFile} = props
+    const {recordingStatus, setRecordingStatus} = useState('inactive')
+    const {audioChunks, setAudioChunks} = useState([])
+    const {duration, setDuration} = useState(0)
+    const mediaRecorder = useRef(null)
+    const mimeType='audio/webm'
+
+    async function startRecording(){
+        let tempStream
+        console.log('Start Rec')
+
+        try{
+            const streamData = navigator.mediaDevices.getUserMedia({audio:true, video:false})
+            tempStream = streamData
+        }catch (err){
+            console.log(err.message)
+            return
+        }
+        setRecordingStatus('recording')
+
+        // create new media recorder instance using stream
+        const media = new MediaRecorder(tempStream, {type:mimeType})
+        mediaRecorder.current = new MediaRecorder(tempStream, {type:mimeType})
+        mediaRecorder.current.start()
+        let localAudioChunks = []
+        mediaRecorder.current.ondataavailable = (event) =>{
+            if (typeof event.data === 'undefined') {return }
+            if (event.data.size  === 0) {return }
+            localAudioChunks.push(event.data)
+        }
+        setAudioChunks(localAudioChunks)
+    }
+
+    async function stopRecording(){
+        setRecordingStatus('inactive')
+        console.log("Stop Rec")
+        mediaRecorder.current.stop()
+        mediaRecorder.current.onstop = () => {
+            const audioBlob = new Blob(audioChunks, {type:mimeType})
+            setAudioStream(audioBlob)
+            setAudioChunks([])
+            setDuration(0)
+        }
+    }
+    useEffect(()=>{
+        if (recordingStatus === 'inactive') {return}
+        const interval = setInterval(()=> {
+            setDuration(curr => curr +1)
+        }, 1000)
+        return () => clearInterval(interval)
+    })
+
     return (
-        <main className='flex-1 p-4 flex flex-col gap-3 sm:gap-4 md:gap-5 justify-center'>
-            <h1 className=''>Free<span className='text-blue-400'>Scribe</span></h1>
+        <main className='flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4 md:gap-5 justify-center pb-20'>
+            <h1 className='font-semibold md:text-7xl sm:text-6xl'>Free<span className='text-blue-400'>Scribe</span></h1>
+            <h3 className='font-medium md:text-lg'>Record<span className='text-blue-400'>&rarr;</span>Transcribe<span className='text-blue-400'>&rarr;</span>Translate</h3>
+            <button onClick={recordingStatus ==='recording' ? stopRecording : startRecording} className='flex specialBtn px-4 px-2 rounded-xl items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4'>
+                <p>{recordingStatus==='inactive' ? 'Record' : 'Stop recording'} </p>
+                <div className='flex item-center gap-2'>
+                    {duration && 
+                    (<p className='text-sm'>${duration}s</p>)}
+                </div>
+                <i className={"fa-solid duration-200 fa-microphone"+ (recordingStatus==='recording' ? ' text-rose-300' : "")}></i>
+            </button>
+            <p className='text-base'>
+                Or <label className='text-blue-400 cursor-pointer hover:text-blue-600 duration-200'>
+                    Upload 
+                    <input onChange={(e) => {
+                        const tempFile = e.target.files[0]
+                        setFile(tempFile)
+                    }} className='hidden' type='file' accept='.mp3,.wave' />
+                </label> a mp3 file.
+            </p>
+            <p className='italic text-slate-300'>Free now, free forever.</p>
         </main>
     )
 }
